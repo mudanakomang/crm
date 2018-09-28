a2de9080d19053fb4fe9079bd8bfc3a9fe11716a
mudanakomang <mudanakomang@hotmail.com>
2018-09-28 11:08:24 +0800

    Added ProfilesFolio Model

---
+++ app/ProfileFolio.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProfileFolio extends Model
+{
+    //
+    protected $table='profilesfolio';
+    protected $fillable=['profileid','folio_master','folio','foliostatus'];
+}
